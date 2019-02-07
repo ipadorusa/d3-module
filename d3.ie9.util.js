1cb8da2aa5133de67b8a3f1d29df128a7b5ba8d8
ipadorusa <ipadorusa@gmail.com>
2019-02-07 19:37:43 +0900

    all commit

---
+++ d3.ie9.util.js
@@ -0,0 +1,15 @@
+var d3_style_prototype   = this.CSSStyleDeclaration.prototype;
+var d3_style_setProperty = d3_style_prototype.setProperty;
+d3_style_prototype.setProperty = function(name, value, priority) {
+	try {
+		d3_style_setProperty.call(this, name, value + "", priority);
+	} catch(exception) {
+		console.warn("d3_style_prototype.setProperty - name: ", name, ", value: ", value, ", priority: ", priority, ", exception: ", exception);
+		switch(name) {
+			case    'left':  name = 'start';  break;
+			case    'right': name = 'end';    break;
+			default:         name = 'middle'; break;
+		}
+		d3_style_setProperty.call(this, name, value + "", priority);
+	}
+};
